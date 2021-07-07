dfd5e8563b1dba6841ab57e8a1f4539c9543f2ee
sarahartsoe <sarahartsoe@gmail.com>
2021-07-07 17:02:06 -0400

    tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import ColorChangeButton from './ColorChangeButton'
 
 function App() {
+  const [color, setColor] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`react-root ${color}`}>
+      <div className='centered'>
+        <h1>Color Picker</h1>
+        <ColorChangeButton color='red' setColor={setColor} />
+        <ColorChangeButton color='blue' setColor={setColor} />
+        <ColorChangeButton color='yellow' setColor={setColor} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/ColorChangeButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ColorChangeButton (props) {
+  return (
+    <button className={props.color} onClick={() => props.setColor(props.color)}>
+      {props.color}
+    </button>
+  )
+}
+
+export default ColorChangeButton
